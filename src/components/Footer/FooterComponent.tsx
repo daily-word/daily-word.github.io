97a6ab595a67be034b2c4fd139eb15d94a7043c8
Leandro Castro <leandro.castro@labsit.io>
2024-10-24 17:03:19 -0300

    feat: add logo and favicon

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,10 +1,10 @@
 export function FooterComponent() {
   return (
     <div className='bg-primary'>
-      <div className='container mx-auto p-7'>
+      <div className='container mx-auto p-7 flex flex-col items-center'>
+        <img src="/logo_full.png" alt="Daily Word Logo" className='w-48' />
         <p className='text-white text-center'>
-          Come here every day to learn a new word
-          <span className='material-symbols-outlined align-middle ml-2'>menu_book</span>
+          Come here every day to learn a new word :)
         </p>
       </div>
     </div>
