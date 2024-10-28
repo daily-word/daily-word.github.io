f73fac0e66e843a6d5595c7df843e0117f8fdb4f
Leandro Castro <leandro.castro@labsit.io>
2024-10-28 10:40:05 -0300

    feat: change title size

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,13 @@ function App() {
       <div className='flex flex-col h-full justify-between gap-8'>
         <div className='bg-primary text-white py-7'>
           <div className='container mx-auto'>
-            <h3 className='text-5xl sm:text-7xl text-center font-semibold capitalize'>{data.word}</h3>
+            <h3 className='text-7xl text-center font-semibold capitalize'>{data.word}</h3>
             <h1 className='text-center text-base text-secondary'>Daily Word</h1>
           </div>
         </div>
         <div className='flex justify-center container mx-auto sm:px-0 px-4 w-full md:w-[650px] lg:w-[750px] xl:w-[800px]'>
           <div className='shadow-primary shadow-md border-solid border-primary border-[1px] rounded-lg'>
-            <h4 className='text-3xl text-white bg-primary p-7 rounded-t-lg'>Meanings</h4>
+            <h4 className='text-2xl text-white bg-primary p-7 rounded-t-lg'>Meanings</h4>
             <ul className='p-7'>
               {data.meanings.map((meaning) => (
                 <li className='text-dark text-lg mb-4 last:mb-0' key={meaning.meaning}>
@@ -41,7 +41,7 @@ function App() {
                     <span className='material-icons-outlined mr-1 align-sub text-primary'>turned_in</span>{meaning.meaning}
                   </p>
                   <p className='text-sm italic font-light'>
-                    "{meaning.example}""
+                    "{meaning.example}"
                   </p>
                 </li>
               ))}
